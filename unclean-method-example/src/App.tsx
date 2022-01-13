1f36485d5d6550ad7908c8fbbe865d5b1c4c070d
CarlosZoft <carlosoftwar@gmail.com>
2022-01-13 20:23:16 -0300

    feat: set up project

---
+++ unclean-method-example/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bad example of unclean method.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
